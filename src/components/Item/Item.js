1306f5db8ba0b4e7c9bfb26aa5b88c3b3bd2aded
federicocipolatti <federicocipolatti@gmail.com>
2022-02-11 14:16:34 -0300

    ItemList/Item OK

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -7,7 +7,7 @@ export const Item = ({item}) => {
 
     return <div className='Item'>
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={item.img}alt="img-item"/>
+            <Card.Img variant="top" src={item.img} alt="img-item"/>
             <Card.Body>
                 <Card.Title>{item.titulo}</Card.Title>
                 <Card.Subtitle>{item.subtitulo}</Card.Subtitle>
