eafaedcb8d203136de248dec16a4900e14e8c10a
federicocipolatti <federicocipolatti@gmail.com>
2022-02-25 18:18:43 -0300

    eventos

--- src/components/ItemDetailContainer/ItemDetail/ItemDetail.js
+++ src/components/ItemDetailContainer/ItemDetail/ItemDetail.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React { useState } from 'react';
 import './ItemDetail.css';
 import { Card } from 'react-bootstrap';
 import { ItemCount } from '../../ItemCount/ItemCount';
 
+const InputCount = ({onConfirm, stock, initial=0}) => {
+    const [count, setCount] = useState(initial)
+
+    const handleChange = ({target}) => {
+        if(target.value <= stock && target.value >= 0) {
+            setCount(target.value)
+        }
+    }
+
+    return (
+        <div>
+            <input type='number' onChange={handleChange} value={count}/>
+            <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+const ButtonCount = ({ onConfirm, stock, initial = 0 }) => {
+    const [count, setCount] = useState(initial)
+
+    const increment = () => {
+        if(count < stock) {
+            setCount(count + 1)
+        }
+    }
+
+    const decrement = () => {
+        if(count > initial) {
+            setCount(count - 1)
+        }
+    }
+
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={decrement}>-</button>
+            <button onClick={increment}>+</button>
+            <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
+        </div>
+    )
+}
+
 export const ItemDetail = ({ product }) => {
 
-    const handleOnAdd = (num) => {
-        console.log(`Se agregaron ${num} productos`);
-      }
+    const optionSelected = (value) => {
+        console.log(value)
+        setOption(value)
+    }
+
+    const Count = inputType === "button" ? ButtonCount : InputCount
+
+    const onConfirm = () => {
+        console.log('agregue al carrito')
+    }
 
     return (
         <div className='ItemDetail'>
@@ -16,10 +65,12 @@ export const ItemDetail = ({ product }) => {
                 <Card.Body className='card-body'>
                     <Card.Title className='card-title'>{product.titulo}</Card.Title>
                     <Card.Subtitle className='card-subt'>{product.subtitulo}</Card.Subtitle>
+                    <Select options={options} onSelect={optionSelected} defaultOption={1}/>
                     <Card.Text className='card-text'>
                         {product.precio}
                     </Card.Text>
-                <ItemCount product={product} initial={1} onAdd={handleOnAdd}/>
+                <Count onConfirm={onConfirm} stock={stock} inicial={1}/>
+                <h3>El valor del select es {option}</h3>
                 </Card.Body>
             </Card>  
         </div>
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -9,6 +9,7 @@ export const ItemDetailContainer = () => {
 
     const [product, setProduct] = useState({}); 
     const [loading, setLoading] = useState(true); 
+    const [counter, setCounter] = useState('button')
     const {productId} = useParams(); 
 
     useEffect(() => {
@@ -24,12 +25,21 @@ export const ItemDetailContainer = () => {
             })
     },[productId])
 
+    const handleCount = () => {
+        if(counter === 'button'){
+            setCounter('input')
+        } else {
+            setCounter('button')
+        }
+    }
+
     return (
         <div className="ItemListContainer">
+            <button onClick={handleCount}>Cambiar count</button>
             {loading ? (
                 <Spinner animation="border"/>
             ) : (
-                <ItemDetail product={product}/>
+                <ItemDetail product={product} inputType={counter}/>
             )}
         </div>
     )
